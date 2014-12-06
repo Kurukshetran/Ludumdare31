2fae1147017bfefcbeb8f52407edf36ef48e3eee
samoatesgames <sam@samoatesgames.com>
2014-12-06 19:35:09 +0000

    [ADD] Round countdown test.
    [ADD] Score.

--- Unity/Assets/Resources/Scripts/GameManagement/GameManager.cs
+++ Unity/Assets/Resources/Scripts/GameManagement/GameManager.cs
@@ -11,6 +11,16 @@ public class GameManager : MonoBehaviour
     public float RoundCooldownLength = 5.0f;
 
     /// <summary>
+    /// All Text components used to show the round start and countdown text.
+    /// </summary>
+    public UnityEngine.UI.Text[] HUDRoundStartText = null;
+
+    /// <summary>
+    /// All Text components used to show the round start and countdown text.
+    /// </summary>
+    public UnityEngine.UI.Text[] HUDScoreText = null;
+
+    /// <summary>
     /// The parent transform to all spawned characters
     /// </summary>
     public Transform CharacterContainer = null;
@@ -40,6 +50,11 @@ public class GameManager : MonoBehaviour
     /// </summary>
     private int m_currentRound = 0;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    private int m_score = 0;
+
 	// Use this for initialization
 	void Start () 
     {
@@ -54,10 +69,32 @@ public class GameManager : MonoBehaviour
 
 	void FixedUpdate () 
     {
+        UpdateScore();
+
 	    if (m_roundEndTime != -1.0f)
         {
+            int time = (int)(this.RoundCooldownLength - (Time.time - m_roundEndTime));
+            foreach (var textcomponent in HUDRoundStartText)
+            {
+                textcomponent.gameObject.SetActive(true);
+
+                if (time != 0)
+                {
+                    textcomponent.text = time.ToString();
+                }
+                else
+                {
+                    textcomponent.text = string.Format("Starting Round {0}...", (m_currentRound + 1));
+                }
+            }
+
             if (Time.time - m_roundEndTime >= this.RoundCooldownLength)
             {
+                foreach (var textcomponent in HUDRoundStartText)
+                {
+                    textcomponent.gameObject.SetActive(false);
+                }
+
                 m_roundEndTime = -1.0f;
                 SpawnWave();
             }
@@ -73,8 +110,17 @@ public class GameManager : MonoBehaviour
             }
         }
 
+        m_score += m_spawnedMonsters.Count * 666;
         m_roundEndTime = Time.time;
 	}
+
+    private void UpdateScore()
+    {
+        foreach (var scoreText in HUDScoreText)
+        {
+            scoreText.text = "Score " + m_score.ToString("D7");
+        }
+    }
 
     private void SpawnWave()
     {
@@ -84,9 +130,7 @@ public class GameManager : MonoBehaviour
         }
         m_spawnedMonsters.Clear();
 
-        Debug.Log("Spawning new wave...");
         m_currentRound++;
-
         int waveSize = Random.Range(4, 5 + m_currentRound);
 
         for (int monsterIndex = 0; monsterIndex < waveSize; ++monsterIndex)
