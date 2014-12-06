31c09d3d8d3ff87f4243a8b657eac18ec4fbf709
samoatesgames <sam@samoatesgames.com>
2014-12-06 16:45:22 +0000

    [ADD] Sound effects and menu theme.
    [FIX] Sexist gameplay.

--- Unity/Assets/Resources/Scripts/Character/ICharacter.cs
+++ Unity/Assets/Resources/Scripts/Character/ICharacter.cs
@@ -22,7 +22,22 @@ using System.Collections.Generic;public abstract class ICharacter : MonoBehavi
     /// <summary>
     /// The container for all spawned bullets
     /// </summary>
-    public Transform BulletContainer = null;    /// <summary>    /// The animation controller for this character    /// </summary>    protected ICharacterAnimationController m_animationController = null;    /// <summary>    /// The rigid body of the character    /// </summary>    protected Rigidbody2D m_body = null;
+    public Transform BulletContainer = null;
+
+    /// <summary>
+    /// The audio clip to be played on attack
+    /// </summary>
+    public AudioClip AttackAudio = null;
+
+    /// <summary>
+    /// The audio clip to be played when hurt
+    /// </summary>
+    public AudioClip HurtAudio = null;    /// <summary>    /// The animation controller for this character    /// </summary>    protected ICharacterAnimationController m_animationController = null;    /// <summary>    /// The rigid body of the character    /// </summary>    protected Rigidbody2D m_body = null;
+
+    /// <summary>
+    /// The audio source for this character
+    /// </summary>
+    protected AudioSource m_audioSource = null;
 
     /// <summary>
     /// The bullet this character uses
@@ -31,6 +46,13 @@ using System.Collections.Generic;public abstract class ICharacter : MonoBehavi
     /// 
     /// </summary>
     protected Dictionary<SpriteRenderer, int> m_initialSortingOrders = new Dictionary<SpriteRenderer, int>();	// Use this for initialization    protected virtual void Start()     {        m_body = this.GetComponent<Rigidbody2D>();        if (m_body == null)        {            Debug.LogError(string.Format("The character {0} does not have a rigid body 2d component.", this.name));            this.gameObject.SetActive(false);        }        m_animationController = this.GetComponent<ICharacterAnimationController>();        if (m_animationController == null)        {            Debug.LogError(string.Format("The character {0} does not have an animation controller component.", this.name));            this.gameObject.SetActive(false);        }
+
+        m_audioSource = this.GetComponent<AudioSource>();
+        if (m_audioSource == null)
+        {
+            Debug.LogError(string.Format("The character {0} does not have an audio source component.", this.name));
+            this.gameObject.SetActive(false);
+        }
 
         // Create bullets        for (int bulletIndex = 0; bulletIndex < this.MaximumNumberOfBullets; ++bulletIndex)
         {
@@ -55,11 +77,25 @@ using System.Collections.Generic;public abstract class ICharacter : MonoBehavi
             return;
         }
 
-        bullet.Fire(this.transform.position, direction);    }    public virtual void Damage(float amount)
+        bullet.Fire(this.transform.position, direction);
+
+        if (!m_audioSource.isPlaying)
+        {
+            m_audioSource.clip = this.AttackAudio;
+            m_audioSource.Play();
+        }    }    public virtual void Damage(float amount)
     {
         this.Health -= amount;
         if (this.Health <= 0.0f)
         {
             this.gameObject.SetActive(false);
+        }
+        else
+        {
+            if (!m_audioSource.isPlaying)
+            {
+                m_audioSource.clip = this.HurtAudio;
+                m_audioSource.Play();
+            }
         }
     }}
--- Unity/Assets/Resources/Scripts/Character/MeleeCharacter/MeleeCharacter.cs
+++ Unity/Assets/Resources/Scripts/Character/MeleeCharacter/MeleeCharacter.cs
@@ -11,16 +11,37 @@
     public float DamagePerHit = 1.0f;	// Use this for initialization	protected override void Start ()     {        base.Start();	}		// Update is called once per frame    protected override void Update()    {        base.Update();	}    // Fixed updates is called once per physics update    protected override void FixedUpdate()    {        base.FixedUpdate();
 
         var isAgroed = false;
-        var isAttacking = false;        var players = GameObject.FindGameObjectsWithTag("Player");        foreach (var player in players)        {            var toPlayerVector = player.transform.position - this.transform.position;            var distance = toPlayerVector.magnitude;
+        var isAttacking = false;
 
-            if (distance <= this.AttackRange)
+        GameObject closestPlayer = null;
+        float distanceToClosestPlayer = float.MaxValue;        var players = GameObject.FindGameObjectsWithTag("Player");        foreach (var player in players)        {
+            var toPlayerVector = player.transform.position - this.transform.position;
+            var distance = toPlayerVector.magnitude;            if (distance <= distanceToClosestPlayer)
             {
-                isAttacking = true;
-                var character = player.GetComponent<ICharacter>();
-                character.Damage(this.DamagePerHit);
-                break;
-            }            if (distance <= this.AgroRange)            {                var movement = toPlayerVector.normalized;                this.transform.position = this.transform.position + (movement * this.Speed);                isAgroed = true;                break;            }        }        var animController = GetAnimationController();
+                closestPlayer = player;
+                distanceToClosestPlayer = distance;
+            }
+        }
+
+        if (distanceToClosestPlayer <= this.AttackRange)
+        {
+            isAttacking = true;
+            var character = closestPlayer.GetComponent<ICharacter>();
+            character.Damage(this.DamagePerHit);
+        }
+        else if (distanceToClosestPlayer <= this.AgroRange)
+        {
+            var toPlayerVector = closestPlayer.transform.position - this.transform.position;
+            var movement = toPlayerVector.normalized;
+            this.transform.position = this.transform.position + (movement * this.Speed);
+            isAgroed = true;
+        }        var animController = GetAnimationController();
         if (isAttacking)
         {
             animController.PlayAttackAnimation();
+            if (!m_audioSource.isPlaying)
+            {
+                m_audioSource.clip = this.AttackAudio;
+                m_audioSource.Play();
+            }
         }        else if (isAgroed)        {            animController.PlayWalkAnimation();        }        else        {            animController.PlayIdleAnimation();        }            }    /// <summary>    ///     /// </summary>    /// <returns></returns>    private ControllableCharacterAnimationController GetAnimationController()    {        return m_animationController as ControllableCharacterAnimationController;    }}
