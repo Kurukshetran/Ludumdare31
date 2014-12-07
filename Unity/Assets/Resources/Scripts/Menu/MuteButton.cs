97d57e1889a600a80737b8db005538dbc184bc5e
samoatesgames <sam@samoatesgames.com>
2014-12-07 21:45:35 +0000

    Final submit.

--- Unity/Assets/Resources/Scripts/Menu/MuteButton.cs
+++ Unity/Assets/Resources/Scripts/Menu/MuteButton.cs
@@ -1,30 +1,1 @@
-﻿using UnityEngine;using System.Collections;public class MuteButton : MonoBehaviour {
-    /// <summary>
-    /// Is audio muted?
-    /// </summary>
-    private bool m_muted = false;
-
-    /// <summary>
-    /// The key for the mute save option
-    /// </summary>
-    public const string VAR_MUTED = "Muted";
-
-    /// <summary>
-    /// The image used for the mute button
-    /// </summary>
-    private UnityEngine.UI.Image m_speakerIcon = null;    // Use this for initialization	void Start ()     {
-        m_speakerIcon = this.GetComponent<UnityEngine.UI.Image>();
-        if (m_speakerIcon == null)
-        {
-            Debug.LogError(string.Format("The button {0} does not have a speaker icon image component", this.name));
-        }
-
-        MuteAudio(PlayerPrefs.GetInt(MuteButton.VAR_MUTED, 0) == 1);	}	    private void MuteAudio(bool mute)
-    {
-        Camera.main.GetComponent<AudioListener>().enabled = !mute;
-        m_speakerIcon.color = mute ? Color.black : Color.white;
-
-        m_muted = mute;
-        PlayerPrefs.SetInt(MuteButton.VAR_MUTED, mute ? 1 : 0);
-    }    /// <summary>    ///     /// </summary>	public void OnClick()    {
-        MuteAudio(!m_muted);    }}
+﻿using UnityEngine;using System.Collections;public class MuteButton : MonoBehaviour {    /// <summary>    /// Is audio muted?    /// </summary>    private bool m_muted = false;    /// <summary>    /// The key for the mute save option    /// </summary>    public const string VAR_MUTED = "Muted";    /// <summary>    /// The image used for the mute button    /// </summary>    private UnityEngine.UI.Image m_speakerIcon = null;    // Use this for initialization	void Start ()     {        m_speakerIcon = this.GetComponent<UnityEngine.UI.Image>();        if (m_speakerIcon == null)        {            Debug.LogError(string.Format("The button {0} does not have a speaker icon image component", this.name));        }        MuteAudio(PlayerPrefs.GetInt(MuteButton.VAR_MUTED, 0) == 1);	}	    private void MuteAudio(bool mute)    {        Camera.main.GetComponent<AudioListener>().enabled = !mute;        m_speakerIcon.color = mute ? Color.black : Color.white;        m_muted = mute;        PlayerPrefs.SetInt(MuteButton.VAR_MUTED, mute ? 1 : 0);    }    /// <summary>    ///     /// </summary>	public void OnClick()    {        MuteAudio(!m_muted);    }}
