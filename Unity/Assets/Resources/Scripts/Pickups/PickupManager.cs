8742b970badf6503ba11ca8dd3ebd2107c59d60c
samoatesgames <sam@samoatesgames.com>
2014-12-07 18:43:04 +0000

    [ADD] Resurrection pickup.

--- Unity/Assets/Resources/Scripts/Pickups/PickupManager.cs
+++ Unity/Assets/Resources/Scripts/Pickups/PickupManager.cs
@@ -1,51 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;using System.Collections;public class PickupManager : MonoBehaviour {    /// <summary>    /// The health kit prefab    /// </summary>    public GameObject HealthKit = null;
 
-public class PickupManager : MonoBehaviour 
-{
     /// <summary>
-    /// The health kit prefab
+    /// The ressurection kit prefab
     /// </summary>
-    public GameObject HealthKit = null;
+    public GameObject RessurectionKit = null;    /// <summary>    /// Singleton instance    /// </summary>    private static PickupManager s_instance = null;    /// <summary>    /// Public access to the pickup manager instance    /// </summary>    public static PickupManager Instance    {        get { return s_instance; }    }    void Awake()    {        if (s_instance != null)        {            Debug.LogError("Multiple pickup managers exist!");            return;        }        s_instance = this;    }    /// <summary>    /// Add a health pickup if a random number is less than chance    /// </summary>    /// <param name="position"></param>    /// <param name="chance"></param>    public void MaybeAddHealthPickup(Vector3 position, float chance)    {        if (Random.Range(0.0f, 1.0f) > chance)        {            return;        }        var pickup = (GameObject)GameObject.Instantiate(this.HealthKit);        pickup.transform.parent = this.transform;        pickup.transform.position = position;    }
 
     /// <summary>
-    /// Singleton instance
-    /// </summary>
-    private static PickupManager s_instance = null;
-
-    /// <summary>
-    /// Public access to the pickup manager instance
-    /// </summary>
-    public static PickupManager Instance
-    {
-        get { return s_instance; }
-    }
-
-    void Awake()
-    {
-        if (s_instance != null)
-        {
-            Debug.LogError("MUltiple pickup managers exist!");
-            return;
-        }
-
-        s_instance = this;
-    }
-
-    /// <summary>
-    /// Add a health pickup if a random number is less than chance
+    /// Add a resurrection pickup if a random number is less than chance
     /// </summary>
     /// <param name="position"></param>
     /// <param name="chance"></param>
-    public void MaybeAddHealthPickup(Vector3 position, float chance)
+    public bool MaybeAddRessurectionPickup(Vector3 position, float chance)
     {
         if (Random.Range(0.0f, 1.0f) > chance)
         {
-            return;
+            return false;
         }
 
-        var pickup = (GameObject)GameObject.Instantiate(this.HealthKit);
+        var pickup = (GameObject)GameObject.Instantiate(this.RessurectionKit);
         pickup.transform.parent = this.transform;
         pickup.transform.position = position;
-    }
-}
+
+        return true;
+    }}
---
+++ Unity/Assets/Resources/Scripts/Pickups/ResurrectionPickup.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ResurrectionPickup : IPickup
+{
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    // Called when something enters the pickups trigger box
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player" || m_pickedup)
+        {
+            return;
+        }
+
+        for (int playerIndex = 0; playerIndex < (int)PlayerIdentity.Noof; ++playerIndex)
+        {
+            var player = other.transform.parent.FindChild( ((PlayerIdentity)playerIndex).ToString() );
+            if (player.gameObject.activeSelf == false)
+            {
+                player.gameObject.SetActive(true);
+            }
+
+            var character = player.GetComponent<ICharacter>();
+            character.ResetHealth();
+        }
+
+        base.OnTriggerEnter2D(other);
+    }
+}
