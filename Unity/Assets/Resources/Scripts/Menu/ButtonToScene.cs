a527a21375d35f61f6c3ad319e51a32ebf46a7f9
samoatesgames <sam@samoatesgames.com>
2014-12-07 09:43:59 +0000

    [MOD] Menu theme.
    [MOD] All SFX should be 2d audio.
    [ADD] Main menu
    [ADD] Local highscore

---
+++ Unity/Assets/Resources/Scripts/Menu/ButtonToScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonToScene : MonoBehaviour 
+{
+    /// <summary>
+    /// The scene to navigate too
+    /// </summary>
+    public string NavigateToScene = null;
+
+    public void OnPress()
+    {
+        Application.LoadLevel(this.NavigateToScene);
+    }
+}
---
+++ Unity/Assets/Resources/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class MainMenu : MonoBehaviour 
+{
+    public const string VAR_HIGHSOCRE = "Highscore";
+
+    /// <summary>
+    /// All labels used to display the highscore
+    /// </summary>
+    public UnityEngine.UI.Text[] m_highscoreLabels = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Load highscore
+        var highscore = PlayerPrefs.GetInt(VAR_HIGHSOCRE, 0);
+
+        foreach (var label in m_highscoreLabels)
+        {
+            label.text = string.Format("HighScore {0}", highscore.ToString("D7"));
+        }
+	}
+}
